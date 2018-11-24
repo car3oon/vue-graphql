a1fcd4239da6c27d965b14b9e65337819c3d42ef
Łukasz Węgiel <car3on@gmail.com>
2018-11-24 14:47:16 +0100

    Add signupUser Mutation

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require("path");
 
 const filePath = path.join(__dirname, "typeDefs.gql");
 const typeDefs = fs.readFileSync(filePath, "utf-8");
+const resolvers = require("./resolvers");
 
 require("dotenv").config({ path: "variables.env" });
 const User = require("./models/User");
@@ -22,6 +23,7 @@ mongoose.set("useCreateIndex", true);
 
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
   context: {
     User,
     Post
